3dcb2e4775b7b7b527b70cbca5b3ff23514e745c
dimat220 <84031369+dimat220@users.noreply.github.com>
2021-05-11 23:20:11 +0300

    Add files via upload

--- src/main/java/Store.java
+++ src/main/java/Store.java
@@ -1,12 +1,11 @@
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 
 public class Store {
 
-    enum MenuStatus {
-        MAIN_MENU,
-
-    }
+    static final String CAR_HEADER = "CarID | CarType | Brand | Model | Price";
+    static final String CAR_ENTRY_TEMPLATE = "%d | %s | %s | %s | %d";
     static String url = "jdbc:sqlite:D:\\DimaStudy\\Databases\\cardealership";
     static DBConnectionManager connMgr;
 
@@ -14,7 +13,6 @@ public class Store {
         ConsoleUI.start(url);
         connMgr = new DBConnectionManager(url);
 
-        MenuStatus st = MenuStatus.MAIN_MENU;
         while(true){
             int cmd = ConsoleUI.getCommand();
 
@@ -35,12 +33,25 @@ public class Store {
                 case 0:
                     return;
             }
+            ConsoleUI.getLineWith("Press 'Enter' to continue...");
         }
     }
 
     private static void showCars() throws SQLException
     {
-        connMgr.showTable("Car");
+        ResultSet res = connMgr.showTable("Car");
+        int count = 0;
+        ConsoleUI.printLine(CAR_HEADER);
+        while (res.next()) {
+            int carID = res.getInt("CarID");
+            CarType carType = CarType.values()[res.getInt("CarType")];
+            String brand = res.getString("Brand");
+            String model = res.getString("Model");
+            int price = res.getInt("Price");
+            ConsoleUI.printLine(String.format(CAR_ENTRY_TEMPLATE, carID, carType.ordinal() == 0 ? 'p' : 't' , brand, model, price));
+            count++;
+        }
+        ConsoleUI.printLine("Total " + count + " cars");
     }
 
     private static void addCar() {
